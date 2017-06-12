6dbc6595ab5af1f87fc74016e72e2b0a68ba9b27
tsh <tian_tsh@163.com>
2017-06-12 22:38:10 +0800

    第一次版本提交

---
+++ syslog/syslog.h
@@ -0,0 +1,15 @@
+#ifndef _SYS_DEFINE_
+#define _SYS_DEFINE_
+
+#include <stdio.h>
+
+#define LOG_LEVEL_CLOSE 0
+#define LOG_LEVEL_ERROR 1
+#define LOG_LEVEL_INFO  2
+#define LOG_LEVEL_DEBUG 3
+
+
+
+
+
+#endif
