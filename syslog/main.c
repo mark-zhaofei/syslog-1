f7ec4c3e9ebd4b660771c0e03a5d34f11da3924f
tsh <tian_tsh@163.com>
2017-06-14 22:27:18 +0800

    在main中添加了测试用例

--- syslog/main.c
+++ syslog/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
+//#define SYS_LOG_DETAIL  //打印时间，函数名等详细信息
+#include "syslog.h"
+
+#define SYS_LOG_TAG  "MAIN.C"
 
 
 int main()
 {
+	//不带可变参数
+	LOGE("错误输出日志\n");
+	LOGI("普通信息输出日志\n");
+	LOGD("调试信息输出日志\n");
 
+	//带可变参数
+	LOGE("错误输出日志:%d\n",100);
+	LOGI("普通信息输出日志:%d\n",200);
+	LOGD("调试信息输出日志:%d\n",300);
 
+	system("pause");
 	return 0;
 }
